275dac78b55e6e82397d942ee333e930eecf5603
Madeline Jimenez <madelineijimenez@gmail.com>
2020-03-27 19:46:00 -0400

    Add search by last name

--- employee_directory/src/components/Search.js
+++ employee_directory/src/components/Search.js
@@ -4,6 +4,7 @@ import Row from "./Row";
 import Col from "./Col";
 import Card from "./Card";
 import EmployeesResults from "./EmployeesResults";
+import SearchForm from "./SearchForm";
 import API from "../utils/API";
 
 class Search extends Component {
@@ -20,82 +21,51 @@ class Search extends Component {
       .catch(err => console.log(err));
   }
 
-  getRandomUser = query => {
-    API.search(query)
-      .then(res => this.setState({ result: res.data }))
+  // Search by name function will change results state to name entered
+  searchByName = () => {
+    API.getRandomUser()
+      .then(res => this.setState({ results: res.data }))
       .catch(err => console.log(err));
   };
 
-//   // Grab the input value and set the search state to this value
-//   handleInputChange = event => {
-//     const value = event.target.value;
-//     const name = event.target.name;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
+  // When the user inputs a name, it changes the search state to this value
+  handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({
+      search: value
+    });
+  };
 
-//   // On submit of the name search, search API by input value
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     this.getRandomUser(this.state.search);
-//   };
+  // When search button is clicked, change results to last name searched
+  handleFormSubmit = event => {
+    if (this.state.search.trim() === "") {
+      return;
+    }
+    let employees = this.state.result.filter(employee => employee.name.last);
+    this.setState({
+      result: employees
+    });
+    console.log(employees);
+  };
 
-  // render() {
-  //   return (
-  //     <div>
-  //         <h1 className="text-center">Employee Directory</h1>
-  //         <p className="text-center">Filter employees by alphabetical order or search by letter.</p>
-          
-  //         <button>Sort up</button>
-  //         <button>Sort down</button>
-          
-  //           <form className="search">
-  //               <div className="form-group">
-  //                   <label htmlFor="name">Search by Name:</label>
-  //                   <input
-  //                   value={this.state.search} onChange={this.handleInputChange} name="name" list="names" type="text" className="form-control" placeholder="Type in a last name to begin" id="name"
-  //                   />
-  //                   <button type="submit" onClick={this.handleFormSubmit} className="btn btn-success">
-  //                   Search
-  //                   </button>
-  //               </div>
-  //           </form>
 
-  //     </div>
-  //   );
-  // }
 
   render() {
     return (
     <Container>
       <Row>
         <Col size="md-8">
-          <Card>
+          <Card title={"Employee Directory"}>
             <EmployeesResults employees={this.state.results} />
-            {/* <table>
-             <thead>
-                 <tr>
-                     <th>First Name</th>
-                    <th>Last Name</th>
-                     <th>Email</th>
-                 </tr>
-              </thead>
-             <tbody>
-                {this.state.results.map((e, i) => {
-                  return (<tr key={i}>
-                      <td>{e.name.first}</td>
-                      <td>{e.name.last}</td>
-                      <td>{e.email}</td>
-                  </tr>)
-                  })}
-              </tbody>
-          </table> */}
           </Card>
         </Col>
         <Col size="md-4">
-          <Card heading="Search">
-
+          <Card heading="Search" title={"Search"}>
+            <SearchForm
+              value={this.state.search}
+              handleInputChange={this.handleInputChange}
+              handleFormSubmit={this.handleFormSubmit}
+            />
           </Card>
         </Col>
       </Row>
